b5df0bea324f94486e258ded2a9dedfb46591bbb
Tarun Singh <tarun.singh1120@gmail.com>
2024-02-07 00:03:28 +0530

    typo in import statement

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
-import { inject } from "@vercel/analytics/";
+import { inject } from "@vercel/analytics";
 
 if (environment.production) {
   enableProdMode();
